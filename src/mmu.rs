e4dbc99f291fd8af81ac26289fa5177fd21b4123
Piotr Rybka <piotr.rybka@nokia.com>
2023-10-12 13:48:52 +0200

    update mmu

--- src/mmu.rs
+++ src/mmu.rs
@@ -1,23 +1,35 @@
-const WRAM_SIZE: usize = 0x8000;
-const HRAM_SIZE: usize = 0x7F;
+const WRAM_SIZE: usize = 0xFFFF;
 
 /// # MMU
 /// - Working RAM 8 KiB
 /// - Video RAM 8 KiB
 pub struct MMU {
-    wram: [u8; WRAM_SIZE],
-    hram: [u8; HRAM_SIZE],
+    wram: [u8; WRAM_SIZE], // For now all 64kB is available
 }
 
 impl MMU {
     pub fn new() -> Self {
         MMU {
             wram: [0xFF; WRAM_SIZE],
-            hram: [0xFF; HRAM_SIZE],
+        }
+    }
+
+    fn get_memory(&self, address: u16) -> &[u8] {
+        match address {
+            _ => &self.wram,
         }
     }
 
     pub fn read_byte(&self, address: u16) -> u8 {
+        todo!();
+    }
+    pub fn write_byte(&self, address: u16, value: u8) {
+        todo!();
+    }
+    pub fn read_word(&self, address: u16) -> u16 {
+        todo!();
+    }
+    pub fn write_word(&self, address: u16, value: u16) {
         todo!();
     }
 }
