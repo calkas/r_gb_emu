c6c7eaef0e243149268f1c3899db15f0172981ad
Piotr Rybka <piotr.rybka@nokia.com>
2023-11-21 13:30:50 +0100

    Add gb memory map const

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,22 @@
 #![allow(dead_code)]
 
+/// GameBoy Memory Map
 pub mod address {
-    pub const INTF_REGISTER: u16 = 0xFF0F;
+    use std::ops::RangeInclusive;
+
+    pub const CARTRIDGE_ROM: RangeInclusive<u16> = 0x0000..=0x7FFF;
+    pub const VIDEO_RAM: RangeInclusive<u16> = 0x8000..=0x9FFF;
+    pub const CARTRIDGE_RAM: RangeInclusive<u16> = 0xA000..=0xBFFF;
+    pub const WORKING_RAM_BANK_0: RangeInclusive<u16> = 0xC000..=0xCFFF;
+    pub const WORKING_RAM_BANK_1_7: RangeInclusive<u16> = 0xD000..=0xDFFF;
+    pub const ECHO_RAM: RangeInclusive<u16> = 0xE000..=0xFDFF;
+    pub const OAM: RangeInclusive<u16> = 0xFE00..=0xFE9F;
+    pub const NOT_USABLE: RangeInclusive<u16> = 0xFEA0..=0xFEFF;
+    pub const HARDWARE_IO_REGISTERS: RangeInclusive<u16> = 0xFF00..=0xFF7F;
+    pub const HIGH_RAM: RangeInclusive<u16> = 0xFF80..=0xFFFE;
     pub const INTE_REGISTER: u16 = 0xFFFF;
+
+    pub const INTF_REGISTER: u16 = 0xFF0F;
 }
 
 /// ISR_ADDRESS
