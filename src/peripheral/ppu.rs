a34729dd7d0b54f21633415a94e745de95d758c2
Piotr Rybka <piotr.rybka@nokia.com>
2024-01-18 14:39:03 +0100

    ppu next step

--- src/peripheral/ppu.rs
+++ src/peripheral/ppu.rs
@@ -1,5 +1,6 @@
 use self::lcd_monochrome::PaletteRegister;
 use self::lcd_monochrome::{Color, PalleteMode};
+use self::sprite::Attribute;
 
 use super::{HardwareAccessible, IoWorkingCycle};
 use crate::constants::{
@@ -7,17 +8,52 @@ use crate::constants::{
     resolution,
 };
 
+mod graphics {
+    pub const MAX_NUMBER_OF_SPRITES: u16 = 40;
+    pub const MAX_SPRITES_PER_LINE: usize = 10;
+}
+
 /// # LCD Control Register
 #[derive(Clone, Copy, Default)]
 struct LcdControlRegister {
-    lcd_enable: bool,
-    window_tile_map_area: bool,
-    window_enable: bool,
-    bg_window_tile_data_area: bool,
-    bg_tile_map_area: bool,
-    obj_size: bool,
-    obj_enable: bool,
-    bg_window_enable_priority: bool,
+    lcd_enable: bool,                // LCD Display Enable (0=Off, 1=On)
+    window_tile_map_area: bool,      // Window Tile Map Display Select (0=9800-9BFF, 1=9C00-9FFF
+    window_enable: bool,             // Window Display Enable (0=Off, 1=On)
+    bg_window_tile_data_area: bool,  // BG & Window Tile Data Select (0=8800-97FF, 1=8000-8FFF)
+    bg_tile_map_area: bool,          // BG Tile Map Display Select (0=9800-9BFF, 1=9C00-9FFF)
+    obj_size: bool,                  // OBJ (Sprite) Size (0=8x8, 1=8x16)
+    obj_enable: bool,                // OBJ (Sprite) Display Enable (0=Off, 1=On)
+    bg_window_enable_priority: bool, // BG Display (for CGB see below) (0=Off, 1=On)
+}
+
+impl LcdControlRegister {
+    pub fn get_window_tile_map_base_address(&self) -> u16 {
+        if self.window_tile_map_area {
+            return 0x9C00;
+        }
+        0x9800
+    }
+
+    pub fn get_tile_data_base_address(&self) -> u16 {
+        if self.bg_window_tile_data_area {
+            return 0x8000;
+        }
+        0x8800
+    }
+
+    pub fn get_bg_tile_map_base_address(&self) -> u16 {
+        if self.bg_tile_map_area {
+            return 0x9C00;
+        }
+        0x9800
+    }
+
+    pub fn get_sprite_high_size(&self) -> u8 {
+        if self.obj_size {
+            return 16;
+        }
+        8
+    }
 }
 
 impl std::convert::From<u8> for LcdControlRegister {
@@ -69,17 +105,12 @@ impl std::convert::From<LcdControlRegister> for u8 {
 /// # LCD Status Register
 #[derive(Clone, Copy, Default)]
 struct LcdStatusRegister {
-    // Bit 6 - LYC=LY Coincidence Interrupt (1=Enable) (Read/Write)
-    ly_interrupt: bool,
-    // Bit 5 - Mode 2 OAM Interrupt         (1=Enable) (Read/Write)
-    mode_2_interrupt: bool,
-    // Bit 4 - Mode 1 V-Blank Interrupt     (1=Enable) (Read/Write)
-    mode_1_interrupt: bool,
-    // Bit 3 - Mode 0 H-Blank Interrupt     (1=Enable) (Read/Write)
-    mode_0_interrupt: bool,
-    // Bit 2 LYC == LY
-    lyc_flag: bool,
-    // Bit 1-0 - Mode Flag       (Mode 0-3, see below) (Read Only)
+    ly_interrupt: bool,     // Bit 6 - LYC=LY Coincidence Interrupt
+    mode_2_interrupt: bool, // Bit 5 - Mode 2 OAM Interrupt
+    mode_1_interrupt: bool, // Bit 4 - Mode 1 V-Blank Interrupt
+    mode_0_interrupt: bool, // Bit 3 - Mode 0 H-Blank Interrupt
+    lyc_flag: bool,         // Bit 2 - LYC == LY
+    // Bit 1-0 - Mode Flag
     //    0: During H-Blank
     //    1: During V-Blank
     //    2: During Searching OAM
@@ -161,27 +192,59 @@ mod lcd_monochrome {
         }
     }
 }
-#[derive(Clone, Copy)]
-enum PpuState {
-    OamScanMode2,
-    DrawingPixelsMode3,
-    HBlankMode0,
-    VBlankMode1,
-}
 
-impl PpuState {
-    pub fn new() -> Self {
-        Self::OamScanMode2
+mod fsm {
+    #[derive(Clone, Copy)]
+    pub enum PpuState {
+        OamScanMode2 = 2,
+        DrawingPixelsMode3 = 3,
+        HBlankMode0 = 0,
+        VBlankMode1 = 1,
     }
-    pub fn next(self) -> Self {
-        match self {
-            Self::OamScanMode2 => Self::DrawingPixelsMode3,
-            Self::DrawingPixelsMode3 => Self::HBlankMode0,
-            Self::HBlankMode0 => Self::VBlankMode1,
-            Self::VBlankMode1 => Self::OamScanMode2,
+
+    impl PpuState {
+        pub fn new() -> Self {
+            Self::OamScanMode2
+        }
+        pub fn next(self) -> Self {
+            match self {
+                Self::OamScanMode2 => Self::DrawingPixelsMode3,
+                Self::DrawingPixelsMode3 => Self::HBlankMode0,
+                Self::HBlankMode0 => Self::VBlankMode1,
+                Self::VBlankMode1 => Self::OamScanMode2,
+            }
         }
     }
 }
+
+mod sprite {
+    pub struct Attribute {
+        pub priority: bool, // Bit7   OBJ-to-BG Priority (0=OBJ Above BG, 1=OBJ Behind BG color 1-3)
+        pub yflip: bool,    // Bit6   Y flip          (0=Normal, 1=Vertically mirrored)
+        pub xflip: bool,    // Bit5   X flip          (0=Normal, 1=Horizontally mirrored)
+        pub dmg_palette: bool, // Bit4   Palette number  **Non CGB Mode Only** (0=OBP0, 1=OBP1)
+    }
+
+    impl From<u8> for Attribute {
+        fn from(value: u8) -> Self {
+            Self {
+                priority: (value.rotate_left(7) & 1) == 1,
+                yflip: (value.rotate_left(6) & 1) == 1,
+                xflip: (value.rotate_left(5) & 1) == 1,
+                dmg_palette: (value.rotate_left(4) & 1) == 1,
+            }
+        }
+    }
+
+    //Object Attribute Memory (OAM)
+    pub struct Sprite {
+        pub attribute: Attribute, // Byte 3 — Attributes/Flags
+        pub tile_index: u8,       // Byte 2 — Tile Index
+        pub x_position: u8,       // Byte 1 — X Position
+        pub y_position: u8,       // Byte 0 — Y Position
+    }
+}
+
 /// # PPU (Picture Processing Unit)
 /// On Gameboy Classic there's only one way to initialize VRAM - manually copy data with CPU instructions. This is done in bootstrap ROM process:
 pub struct PictureProcessingUnit {
@@ -203,7 +266,9 @@ pub struct PictureProcessingUnit {
     pub vblank_interrupt_req: bool,
     pub lcd_interrupt_req: bool,
     //..::Internal::..
-    ppu_fsm: PpuState,
+    ppu_fsm: fsm::PpuState,
+    internal_scan_line_counter: u32,
+    sprite_buffer: Vec<sprite::Sprite>,
     //..::Out::..
     pub out_frame_buffer: [u8; resolution::SCREEN_W * resolution::SCREEN_W * 3],
 }
@@ -211,8 +276,10 @@ pub struct PictureProcessingUnit {
 impl PictureProcessingUnit {
     pub fn new() -> Self {
         PictureProcessingUnit {
+            //..::Memory::..
             vram: [memory::DEFAULT_INIT_VALUE; memory::VRAM_SIZE],
             voam: [memory::DEFAULT_INIT_VALUE; memory::VOAM_SIZE],
+            //..::Registers::..
             lcd_control_register: LcdControlRegister::default(),
             lcd_stat_register: LcdStatusRegister::default(),
             scy_register: 0,
@@ -226,9 +293,65 @@ impl PictureProcessingUnit {
             wx_register: 0,
             vblank_interrupt_req: false,
             lcd_interrupt_req: false,
-            ppu_fsm: PpuState::new(),
+            //..::Internal::..
+            ppu_fsm: fsm::PpuState::new(),
+            internal_scan_line_counter: 0,
+            sprite_buffer: Vec::new(),
+            //..::Out::..
             out_frame_buffer: [memory::DEFAULT_INIT_VALUE;
                 resolution::SCREEN_W * resolution::SCREEN_W * 3],
+        }
+    }
+
+    fn get_sprite_from_oam(&mut self, sprite_id: u16) -> sprite::Sprite {
+        let oam_base_address = *address::OAM.start();
+        // sprite occupies 4 bytes in the sprite attributes table
+        let sprite_index = sprite_id * 4;
+        let y_position = (self.read_byte_from_hardware_register(oam_base_address + sprite_index)
+            as i8
+            - 16) as u8;
+        let x_position = (self.read_byte_from_hardware_register(oam_base_address + sprite_index + 1)
+            as i8
+            - 8) as u8;
+        let tile_index = self.read_byte_from_hardware_register(oam_base_address + sprite_index + 2);
+
+        let raw_attribute =
+            self.read_byte_from_hardware_register(oam_base_address + sprite_index + 3);
+
+        sprite::Sprite {
+            attribute: Attribute::from(raw_attribute),
+            tile_index,
+            x_position,
+            y_position,
+        }
+    }
+
+    pub fn check_lyc_ly_comparison(&mut self) {
+        if self.ly_register == self.lyc_register {
+            self.lcd_stat_register.lyc_flag = true;
+            self.lcd_stat_register.ly_interrupt = true;
+        }
+    }
+
+    fn sprite_search(&mut self) {
+        self.sprite_buffer.clear();
+
+        let line = self.ly_register;
+        let sprite_high_size = self.lcd_control_register.get_sprite_high_size();
+
+        for sprite_id in 0..graphics::MAX_NUMBER_OF_SPRITES {
+            if self.sprite_buffer.len() == graphics::MAX_SPRITES_PER_LINE as usize {
+                break;
+            }
+
+            let sprite = self.get_sprite_from_oam(sprite_id);
+
+            // The sprite is not on the screen at this line.
+            if line < sprite.y_position || line >= sprite.y_position + sprite_high_size {
+                continue;
+            }
+
+            self.sprite_buffer.push(sprite);
         }
     }
 }
@@ -300,24 +423,53 @@ impl IoWorkingCycle for PictureProcessingUnit {
             return;
         }
 
+        self.internal_scan_line_counter += cycles;
+
         let state = self.ppu_fsm;
+
+        // Mode_2 (80 dots) + Mode_3 (172 dots) + Mode_0 (204 dots) = 456 dots
         match state {
-            PpuState::OamScanMode2 => {
-                self.ppu_fsm.next();
-            }
-            PpuState::DrawingPixelsMode3 => {
-                self.ppu_fsm.next();
-            }
-            PpuState::HBlankMode0 => {
-                //wait, then go back to sprite search for next line, or vblank.
-                if self.ly_register >= 144 {
+            fsm::PpuState::OamScanMode2 => {
+                self.lcd_stat_register.ppu_mode = fsm::PpuState::OamScanMode2 as u8;
+
+                if self.internal_scan_line_counter >= 80 {
+                    self.sprite_search();
+                    self.internal_scan_line_counter -= 80;
                     self.ppu_fsm.next();
-                } else {
-                    self.ppu_fsm = PpuState::OamScanMode2;
                 }
             }
-            PpuState::VBlankMode1 => {
-                self.ppu_fsm.next();
+            fsm::PpuState::DrawingPixelsMode3 => {
+                self.lcd_stat_register.ppu_mode = fsm::PpuState::DrawingPixelsMode3 as u8;
+
+                if self.internal_scan_line_counter >= 172 {
+                    self.internal_scan_line_counter -= 172;
+                    self.ppu_fsm.next();
+                }
+            }
+            fsm::PpuState::HBlankMode0 => {
+                self.lcd_stat_register.ppu_mode = fsm::PpuState::HBlankMode0 as u8;
+
+                if self.internal_scan_line_counter >= 204 {
+                    self.internal_scan_line_counter -= 204;
+
+                    self.ly_register = (self.ly_register + 1) % 154;
+
+                    if self.ly_register >= 144 {
+                        //vblank
+                        self.ppu_fsm.next();
+                    } else {
+                        //go back to sprite search for next line
+                        self.ppu_fsm = fsm::PpuState::OamScanMode2;
+                    }
+                }
+            }
+            fsm::PpuState::VBlankMode1 => {
+                self.lcd_stat_register.ppu_mode = fsm::PpuState::VBlankMode1 as u8;
+                //Duration 4560 dots (10 scanlines)
+                if self.internal_scan_line_counter >= 4560 {
+                    self.internal_scan_line_counter -= 4560;
+                    self.ppu_fsm.next();
+                }
             }
         }
     }
