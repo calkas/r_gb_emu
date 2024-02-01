cd8effb627fee426b727c198d5547fb755fe60d3
Piotr Rybka <piotr.rybka@nokia.com>
2024-02-01 15:39:03 +0100

    test clean up

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,8 @@
-use r_gb_emu::cpu::Cpu;
-use r_gb_emu::iommu::IOMMU;
-use r_gb_emu::peripheral::cartridge::Cartridge;
-use r_gb_emu::peripheral::joypad::JoypadInput;
-use r_gb_emu::peripheral::ppu::PictureProcessingUnit;
 use r_gb_emu::GameBoyEmulator;
-use std::cell::RefCell;
 use std::fs;
 use std::fs::File;
-use std::io::{Error, Write};
+use std::io::Write;
 use std::path::Path;
-use std::rc::Rc;
-
 #[test]
 fn cpu_instruction_behavior_test() {
     let mut gameboy = GameBoyEmulator::new();
@@ -34,9 +26,4 @@ fn cpu_instruction_behavior_test() {
 
     println!("cycles {}", sum_of_cycles);
     println!("Test output: {}", gameboy.serial_out());
-
-    // //63802933 * 4
-    // while sum_of_cycles < 200 {
-    //     sum_of_cycles += gameboy.emulation_step();
-    // }
 }
